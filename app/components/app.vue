3b3aea84987034d31ae9670a4ac48f6d5d65b512
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-03-03 13:44:09 -0600

    Generated html structure and css styles

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,6 +1,82 @@
 <template lang="html">
-  <div class="">
-
+  <div class="app">
+    <nav class="top-nav">
+      <h1>Marvel</h1>
+    </nav>
+    <div class="section">
+      <div class="container">
+        <div class="main">
+          <div class="sidebar">
+            <img class="sidebar__img" src="http://vignette4.wikia.nocookie.net/powerrangers/images/6/6e/Black_Dragon_True_Form.jpg/revision/latest?cb=20161130192633" alt="">
+            <h1 class="sidebar__title">Mysterious Ranger</h1>
+            <h3 class="sidebar__year">2017</h3>
+            <h4 class="sidebar__creators">Creators</h4>
+            <hr/>
+            <ul>
+              <li class="sidebar__creators--item">Creator Name Here</li>
+              <li class="sidebar__creators--item">Creator Name Here</li>
+              <li class="sidebar__creators--item">Creator Name Here</li>
+              <li class="sidebar__creators--item">Creator Name Here</li>
+              <li class="sidebar__creators--item">Creator Name Here</li>
+            </ul>
+          </div>
+          <div class="main-content">
+            <div class="character">
+              <h2 class="character-heading">Characters</h2>
+              <hr/>
+              <div class="grid">
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h4 class="grid-item__name">Thor</h4>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h4 class="grid-item__name">Thor</h4>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h4 class="grid-item__name">Thor</h4>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h4 class="grid-item__name">Thor</h4>
+                </div>
+              </div>
+            </div>
+            <div class="comics">
+              <h2 class="comics-heading">Comics</h2>
+              <hr/>
+              <div class="grid">
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h3 class="comics-number">#7</h3>
+                  <h5 class="grid-item__name">Thor</h5>
+                  <button class="comics-btn">Read More</button>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h3 class="comics-number">#7</h3>
+                  <h5 class="grid-item__name">Thor</h5>
+                  <button class="comics-btn">Read More</button>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h3 class="comics-number">#7</h3>
+                  <h5 class="grid-item__name">Thor</h5>
+                  <button class="comics-btn">Read More</button>
+                </div>
+                <div class="grid-item">
+                  <img src="https://i.annihil.us/u/prod/marvel/i/mg/5/a0/537bc7036ab02/standard_xlarge.jpg" alt="">
+                  <h3 class="comics-number">#7</h3>
+                  <h5 class="grid-item__name">Thor</h5>
+                  <button class="comics-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
